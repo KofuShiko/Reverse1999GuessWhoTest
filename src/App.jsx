ad8690662fa4c20c708d4a678d680320fd719b04
KofuShiko <ryutokiusami@gmail.com>
2026-07-19 01:22:08 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Cards from './Components/Cards'
+import Buttons from './Components/Buttons'
+import { Routes, Route, HashRouter } from "react-router-dom";
+
+
+//import images
+const humanoid = import.meta.glob('./assets/Humanoid/*.png', {eager:true, import:'default'});
+const nonhumanoid = import.meta.glob('./assets/Non-Humanoid/*.png', {eager:true, import:'default'});
+
+//format them
+const format = (char, cat) => {
+  //get file names to sort both folders alphabetically together
+  return Object.keys(char).filter(path => path.endsWith('.png')).map((path, index) => {
+    const basename = path.split('/').pop()
+    const cleanFileName = basename.replace('.png', '');
+  return{
+    id: `${cat}-${index}`,
+    url: char[path].default || char[path],
+    category: cat,
+    fileName: cleanFileName,
+    name: ''
+  }});
+};
+
+function App() {
+  //sets active images to show
+  const [images, setImages] = useState([]);
+  //excludes categories
+  const [excludeCat, setExcludeCat] = useState([]);
+  //excludes cards
+  const [exclude, setExclude] = useState([]);
+  //to hide excluded cards
+  const [hide, setHide] = useState(false);
+
+  const categoryNames = ['Humanoid', 'Non-Humanoid'];
+  //fetch the names file
+  useEffect(() => {
+    fetch('/names.json')
+      .then((res) => res.json())
+      .then((namesArray) => {
+        //mix images of both folders
+        const allImages = [
+          ...format(humanoid, 'Humanoid'),
+          ...format(nonhumanoid, 'Non-Humanoid')
+        ];
+
+        //sorting images alphabetically
+        const sortedImages = allImages.sort((a,b) => {
+          const nameA = a.fileName || '';
+          const nameB = b.fileName || '';
+          return nameA.localeCompare(nameB, undefined, {numeric: true, sensitivity: 'base'})
+        });
+
+        //putting names on the images
+        const imagewNames = sortedImages.map((img, index) => ({
+          ...img, 
+          name: namesArray[index] || img.fileName
+        }));
+        //set all images to show
+        setImages(imagewNames);
+      })
+      .catch( (err) => console.error('Error fetching names', err));
+    }, []);
+
+  //toggles which category to exclude
+  const toggleExcludeCat = (category) => {
+    setExcludeCat((prev) => prev.includes(category) ? prev.filter((i) => i !== category) : [...prev, category]);
+  }
+  //resets the category filter and the excluded characters
+  const reset = () => {setExcludeCat([]), setExclude([]), setHide(false)};
+  
+  //stores excluded characters
+  const excluded = (id) => {
+    setExclude((prev) => prev.includes(id) ? prev.filter((i) => i !== id) : [...prev, id]);};
+
+  //toggle hide the excluded characters
+  const hideExcluded = () => {
+    setHide((prev) => !prev);
+  }
+
+  const included = images.filter((char) => {
+    const isCategoryExcluded = excludeCat.includes(char.category);
+    const isCardGrayedOut = exclude.includes(char.id);
+    if (isCategoryExcluded) return false;
+    if (hide && isCardGrayedOut) return false;
+    return true;
+  });
+
+  return (
+    <>
+      <div className="bg-black/85 h-screen overflow-y-auto duration-200 ease-in-out transition-all">
+        <Buttons categoryNames={categoryNames} excluded={excludeCat} onToggle={toggleExcludeCat} reset={reset} hideToggle={hideExcluded} hidden={hide}/>
+        <Cards characters={included} exclude={exclude} excluded={excluded}/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Buttons.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+function Buttons({categoryNames, excluded, onToggle, reset, hideToggle, hidden}) {
+    return(
+        <section className="sticky top-0 z-10 flex flex-wrap justify-between gap-3 p-2 py-4 bg-linear-to-b from-black to-transparent text-white/80 text-xs">
+            <div>
+                <span className='text-xl'>Reverse:1999</span>
+                <h1 className="text-xs"> Guess Who</h1>
+            </div>
+            <div className='*:m-1 *:rounded-md *:p-1 *:px-3 *:font-bold *:hover:text-white/90 *:duration-300'>
+            {categoryNames && categoryNames.map((category) => {
+                const excludedCategory = excluded.includes(category);
+                return (
+                <button key={category} onClick = {() => onToggle(category)} className={excludedCategory ? 'bg-black/30 hover:bg-black/50' : 'bg-amber-700 hover:bg-amber-600'}>
+                    {category}
+                </button>
+                );
+            })}
+                <button onClick={hideToggle} className="text-xs">{hidden ? 'Show All' : 'Hide Grayed'}</button>
+            </div>
+            <button onClick={reset} className="rounded-md m-1 p-1 px-3 text-white/80 font-bold duration-300 bg-mist-800 hover:bg-mist-700 hover:text-white/90">
+                Reset
+            </button>
+        </section>
+    )
+}
+
+export default Buttons
