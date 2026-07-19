7b24eafd48ee361a152c0b28d408bcf1763576b0
KofuShiko <ryutokiusami@gmail.com>
2026-07-19 10:57:49 +0800

    visual improvements

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-black/85 h-screen overflow-y-auto">
+      <div className="bg-mist-800 h-screen overflow-y-auto">
         <Buttons categoryNames={categoryNames} excluded={excludeCat} onToggle={toggleExcludeCat} reset={reset} hideToggle={hideExcluded} hidden={hide}/>
         <Cards characters={included} exclude={exclude} excluded={excluded}/>
       </div>
--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react';
 
 function Buttons({categoryNames, excluded, onToggle, reset, hideToggle, hidden}) {
     return(
-        <section className="sticky top-0 z-10 flex flex-wrap justify-between gap-3 p-2 py-4 bg-linear-to-b from-black to-transparent text-white/80 text-xs">
-            <div>
-                <span className='text-xl'>Reverse:1999</span>
-                <h1 className="text-xs"> Guess Who</h1>
+        <section className="sticky top-0 z-10 flex flex-wrap md:justify-between justify-center item-center gap-3 p-2 py-4 bg-linear-to-b from-black to-transparent text-white/80 text-xs">
+            <div className='md:text-left text-center'>
+                <span className='text-xl font-serif'>Reverse:1999</span>
+                <h1 className="text-xs">Guess Who Game</h1>
             </div>
-            <div className='*:m-1 *:rounded-md *:p-1 *:px-3 *:font-bold *:hover:text-white/90 *:duration-300'>
+            <div className='text-center *:m-1 *:rounded-md *:p-1 *:px-3 *:font-bold *:hover:text-white/90 *:duration-300'>
             {categoryNames && categoryNames.map((category) => {
                 const excludedCategory = excluded.includes(category);
                 return (
@@ -16,7 +16,7 @@ function Buttons({categoryNames, excluded, onToggle, reset, hideToggle, hidden})
                 </button>
                 );
             })}
-                <button onClick={hideToggle} className="text-xs">{hidden ? 'Show All' : 'Hide Grayed'}</button>
+                <button onClick={hideToggle} className="text-xs">{hidden ? 'Show All' : 'Hide Excluded'}</button>
             </div>
             <button onClick={reset} className="rounded-md m-1 p-1 px-3 text-white/80 font-bold duration-300 bg-mist-800 hover:bg-mist-700 hover:text-white/90">
                 Reset
